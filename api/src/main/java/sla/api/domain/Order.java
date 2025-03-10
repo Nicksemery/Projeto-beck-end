fc9de30ce9e0acd38432d07e81b2d56930043bd7
Nicolas Rodrigues Emery <Skall@academicos.com>
2025-03-10 16:14:03 -0300

    Novo projeto para relacionamento de banco

---
+++ api/src/main/java/sla/api/domain/Order.java
@@ -0,0 +1,31 @@
+package sla.api.domain;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.HashSet;
+
+@Entity
+@Table (name = "tb_order")
+public class Order {
+
+        @Id
+        @GeneratedValue (strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+        private Instant moment;
+        private OrderStatus status;
+        @ManyToOne
+        @JoinColumn(name = "client_id")
+        private User client;
+
+        @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+        private Payment payment;
+
+        /*
+        @OneToMany(mappedBy = "id.order")
+        private Set<OrderItem> items = new HashSet<>();
+        */
+}
+
---
+++ api/src/main/java/sla/api/domain/OrderStatus.java
@@ -0,0 +1,10 @@
+package sla.api.domain;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+
+}
